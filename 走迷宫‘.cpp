6585b1f2edaa507e19367afd68b8908992d86426
2432312477 <101541803+2432312477@users.noreply.github.com>
2022-03-18 13:21:41 +0800

    Add files via upload

---
+++ 走迷宫‘.cpp
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<time.h>
+char map[9][12] = {
+	"*A*********",
+	"***AAA*AAA*",
+	"AAA**A****A",
+	"*A**AAA*AA*",
+	"***********",
+	"AAAAA*AA*AA",
+	"**A****AA*A",
+	"***A*AA***A",
+	"*A******A*P",
+
+}
+; int curX = 0, curY = 0;
+void printperson()
+{
+	COORD pos;
+	pos.X = curX;
+	pos.Y = curY;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	printf("k");
+
+}
+void printMap() 
+{
+	int i, j;
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 11; j++)
+		{
+			printf("%c", map[i][j]);
+		}
+		printf("\n");
+	}
+}
+void Move(char dir)
+{
+	switch (dir)
+	{
+	case'w':
+			curY--;
+			if (curY < 0)curY = 0;
+			if (map[curY][curX] == 'A')curY++;
+			break;;
+	case's':
+			curY++;
+			if (curY >= 9)curY = 9 - 1;
+			if (map[curY][curX] == 'A')curY--;
+			break;
+	case'a':
+			curX--;
+			if (curX < 0)curX = 0;
+			if (map[curY][curX] == 'A')curX++;
+			break;
+	case'd':
+			curX++;
+			if (curX >= 11)curX = 12 - 1;
+			if (map[curY][curX] == 'A')curX--;
+			break;
+	}
+}
+void main()
+{
+	char dir;
+	while (1)
+	{
+		system("cls");
+		printMap();
+		printperson();
+		dir = _getch();
+		Move(dir);
+		if (map[curY][curX] == 'P')
+		{
+			printf("恭喜，成功通过！");
+			break;
+		}
+	}
+	system("pause");
+}
